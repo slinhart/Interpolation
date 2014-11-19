8a420874374359d51e14282efacf70c9f8f2873c
Jason Balog <jbalog93@gmail.com>
2014-11-19 14:46:59 -0500

    Added more graphs/points. Added parameter to plot3D so that it can accept title names.

--- Plot3D.m
+++ Plot3D.m
@@ -1,8 +1,9 @@
-function Plot3D(xplane, yplane, xi, yi, zi, interp)
+function Plot3D(xplane, yplane, xi, yi, zi, interp, name)
 
 figure
 colormap hsv
 surf(xplane, yplane, interp, 'EdgeColor', 'interp'); hold on
 plot3(xi, yi, zi, 'o');
+title(name);
 
 end
--- VandermondeInterpolation.m
+++ VandermondeInterpolation.m
@@ -1,4 +1,4 @@
-clear; close;
+clear all; close all;
 
 %%% z = x^2 + y^2
 xi = [.15 1.95 4 6.7 7.7 9.1 12 14.3 16.5 19];
@@ -9,7 +9,7 @@ x = linspace(min(xi), max(xi));
 y = linspace(min(yi), max(yi));
 [X, Y] = meshgrid(x, y);
 interp = Interpolate(xi, yi, zi, X, Y);
-Plot3D(X, Y, xi, yi, zi, interp);
+Plot3D(X, Y, xi, yi, zi, interp,'x^2 + y^2');
 %%%
 
 %%%%%% z = sin(1 + sqrt(x^2 + y^2))
@@ -22,7 +22,7 @@ f = @(x,y) sin(1 + sqrt(x.^2 + y.^2));
 zi = f(xi,yi); 
 [X, Y] = meshgrid(x, y);
 interp = Interpolate(xi, yi, zi, X, Y);
-Plot3D(X, Y, xi, yi, zi, interp);
+Plot3D(X, Y, xi, yi, zi, interp, 'Naive: sin(1 + sqrt(x^2 + y^2))');
 %%%
 
 %%% Points strategially chosen between (-4,4)
@@ -34,5 +34,36 @@ f = @(x,y) sin(1 + sqrt(x.^2 + y.^2));
 zi = f(xi,yi); 
 [X, Y] = meshgrid(x, y);
 interp = Interpolate(xi, yi, zi, X, Y);
-Plot3D(X, Y, xi, yi, zi, interp);
+Plot3D(X, Y, xi, yi, zi, interp,'Strategic: sin(1 + sqrt(x^2 + y^2))');
 %%%
+
+%%%%%% z = (x*y*(x^2 - y^2))/(x^2 + y^2)
+%%% 9 strategically chosen points
+xi = [-3.99, -3.89, -3.911, -0.1, 0.13, 0.03, 2.22, 2.34, 3.90];
+yi = [3.93, 0.11, -3.87, 3.01, 0.12, -2.99, 2.05, -2.11, -0.13];
+x = linspace(min(xi), max(xi));
+y = linspace(min(yi), max(yi));
+f = @(x,y) (x .* y .* (x.^2 - y.^2)) ./ (x.^2 + y.^2);
+zi = f(xi,yi); 
+[X, Y] = meshgrid(x, y);
+interp = Interpolate(xi, yi, zi, X, Y);
+Plot3D(X, Y, xi, yi, zi, interp, '9 points: (x*y*(x^2 - y^2))/(x^2 + y^2)');
+%%%
+
+%%% 13 strategically chosen points
+xi = [-3.99, -3.89, -3.911, -2.1, -2.03, -0.1, 0.13, 0.03, 2.22, 2.34, 3.97, 3.90, 3.88];
+yi = [3.93, 0.11, -3.87, 1.99, -1.95, 3.01, 0.12, -2.99, 2.05, -2.11, 3.79, -0.13, -3.967];
+x = linspace(min(xi), max(xi));
+y = linspace(min(yi), max(yi));
+f = @(x,y) (x .* y .* (x.^2 - y.^2)) ./ (x.^2 + y.^2);
+zi = f(xi,yi); 
+[X, Y] = meshgrid(x, y);
+interp = Interpolate(xi, yi, zi, X, Y);
+Plot3D(X, Y, xi, yi, zi, interp, '13 points: (x*y*(x^2 - y^2))/(x^2 + y^2)');
+%%%
+
+
+
+
+
+
