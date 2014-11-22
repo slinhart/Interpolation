518808a544426a3c07f5246d1e68ff7f0e5e79b9
Shayne Linhart <slinhart@knights.ucf.edu>
2014-11-21 21:38:38 -0500

    Added Error analysis section of report. Removed some unwanted leftover code.

--- Plot3D.m
+++ Plot3D.m
@@ -3,8 +3,6 @@ function Plot3D(xplane, yplane, xi, yi, zi, interp, name, realZ)
 figure
 colormap jet
 surf(xplane, yplane, interp, 'EdgeColor', 'interp'); hold on
-freezeColors
-color winter
 if (~isnan(realZ))
    surf(xplane, yplane, realZ, 'EdgeColor', 'interp');
 end
